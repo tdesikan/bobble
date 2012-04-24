7f8cdfc7d12a767883e0c49ed2c339600e3d1d4c
Andrew Farmer <andrew@moovweb.com>
2012-04-24 16:09:18 -0700

    update readme and add BOBBLE_ prefix to environment variables to avoid conflig

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ bobble
 ======
 
 For freely pinging your favorite web services &amp; freely or cheaply getting email/SMS notifications when they&#39;re down.
+
+Set these environment variables for Twilio:
+
+    BOBBLE_TWILIO_SID
+    BOBBLE_TWILIO_TOKEN
+    BOBBLE_TWILIO_FROM_PHONENUMBER
+    BOBBLE_TWILIO_TO_PHONENUMBER
+
+Set these environment variables for Google Voice:
+
+
+Set these environment variables for Gmail:
+
--- lib/bobble/twilio_notifier.rb
+++ lib/bobble/twilio_notifier.rb
@@ -8,8 +8,8 @@ class Bobble::TwilioNotifier
     def create_client
       return if @@client
 
-      account_sid = ENV['TWILIO_SID']
-      auth_token = ENV['TWILIO_TOKEN']
+      account_sid = ENV['BOBBLE_TWILIO_SID']
+      auth_token = ENV['BOBBLE_TWILIO_TOKEN']
 
       @@client = Twilio::REST::Client.new account_sid, auth_token
     end
@@ -18,8 +18,8 @@ class Bobble::TwilioNotifier
       create_client
 
       params = {
-        :from => ENV['TWILIO_PHONENUMBER'],
-        :to => ENV['MY_PHONENUMBER'],
+        :from => ENV['BOBBLE_TWILIO_FROM_PHONENUMBER'],
+        :to => ENV['BOBBLE_TWILIO_TO_PHONENUMBER'],
         :body => message
       }
       @@client.account.sms.messages.create(params)
